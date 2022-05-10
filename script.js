6a211ad39387a93779d6b9b4aac459c23a8caafc
sheinken <heinken.sebastian@gmail.com>
2022-05-10 19:41:28 -0300

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const addProduct = document.getElementsByClassName("add-product");
-console.log(addProduct);
+// const addToCart = document.getElementsByClassName("add-to-cart");
+// console.log(addToCart);
 
-for (let i = 0; i < addProduct.length; i++) {
-    let button = addProduct[i]
-    button.addEventListener("click", function() {
-        console.log("button clicked");
+// for (let i = 0; i < addToCart.length; i++) {
+//     let button = addToCart[i]
+//     button.addEventListener("click", function() {
+//         console.log("button clicked");
+//     })
+// }
+
+// let addToCartButtons = document.getElementsByClassName("add-to-cart")
+// for (let i = 0; i < addToCartButtons.length; i++) {
+//     let button = addToCartButton [i]
+//     button.addEventListner("click", addToCartClicked)
+// }
+
+// function addToCartClicked(event) {
+//     let button = event.target
+// }
+
+let carts = document.querySelectorAll(".product-img");
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener("click", () => {
+        cartNumbers();
     })
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+    console.log(productNumbers);
+    console.log(typeof productNumbers);
+
+    productNumbers = parseInt(productNumbers);
+    console.log(typeof productNumbers);
+    localStorage.setItem("cartNumbers", 1);
+
 }
